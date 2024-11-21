4f311e92bca52d3f3d05eb07441d572266aa7635
MotionBrain <82259763+MtionBra1n@users.noreply.github.com>
2024-11-21 13:27:22 +0100

    Movement und Turn

--- Assets/+++Workdata/Scripts/Player/PlayerController.cs
+++ Assets/+++Workdata/Scripts/Player/PlayerController.cs
@@ -8,43 +8,27 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float movementSpeed = 5f;
-    public float jumpPower = 3f; 
     
     #region Private Variables
 
     public GameInput inputActions;
-    /// <summary>
-    /// reference to the moveaction from the inputmap (WASD)
-    /// </summary>
+    
     private InputAction moveAction;
-    private InputAction jumpAction;
     
     private Vector2 moveInput;
 
     private Rigidbody2D rb;
 
+    private Animator anim;
     #endregion
-    
-    /// <summary>
-    /// 
-    /// </summary>
+
     private void Awake()
     {
-        /*
-         *
-         *
-         *
-         * 
-         */
         rb = gameObject.GetComponent<Rigidbody2D>();
-
-        if (rb == null)
-        {
-            Debug.LogError("Rigidbody not available");
-        }
+        anim = gameObject.GetComponent<Animator>();
+        
         inputActions = new GameInput();
         moveAction = inputActions.Player.Move;
-        jumpAction = inputActions.Player.Jump;
     }
 
     private void OnEnable()
@@ -52,14 +36,13 @@ public class PlayerController : MonoBehaviour
         inputActions.Enable();
         moveAction.performed += Move;
         moveAction.canceled += Move;
-
-        jumpAction.performed += Jump;
     }
 
 
     private void FixedUpdate()
     {
         rb.velocity = new Vector2(moveInput.x * movementSpeed, rb.velocity.y);
+        anim.SetFloat("MovementValue", MathF.Abs(rb.velocity.x));
     }
 
     private void OnDisable()
@@ -71,40 +54,21 @@ public class PlayerController : MonoBehaviour
         
         //
         moveAction.canceled -= Move;
-        
-        jumpAction.performed -= Jump;
     }
 
     private void Move(InputAction.CallbackContext ctx)
     {
         moveInput = ctx.ReadValue<Vector2>();
-    }
-    
-    private void Jump(InputAction.CallbackContext ctx)
-    {
-        rb.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-    }
 
-
-    public void MAthe1()
-    {
-        int result = Addnumbers(4, 6);
-    }
-
-    public int Addnumbers(int a, int b)
-    {
-        int x = a * 2 + b * 2;
-        return x;
-    }
-
-    public bool isDoorOpen;
-    public void CheckIfDoorIsOpen()
-    {
-        if (isDoorOpen == false)
+        if (moveInput.x > 0)
         {
-           //.... 
+            transform.rotation = Quaternion.Euler(0,0,0);
+        }
+        else if (moveInput.x < 0)
+        {
+            transform.rotation = Quaternion.Euler(0,180,0);
         }
     }
-
     
+
 }
